49866d587b017d5bff759c4253346a29bb5a6d26
Lasmatriani <lasmatriani@gmail.com>
2016-04-27 05:04:09 +0700

    Perhitungan1

---
+++ src/tugas/bab5/Perhitungan1.java
@@ -0,0 +1,14 @@
+
+public class Perhitungan {
+
+    public static double penjumlahan(double a, double b) {
+        double c = a + b;
+        return c;
+    }
+
+    public static double pengurangan(double a, double b) {
+        double c = a - b;
+        return c;
+    }
+
+}
